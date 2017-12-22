20564547036829f4be1a1b3e4d437edcb8cbf03f
Suryanand Singh <singhsuryanand@gmail.com>
2017-12-22 12:16:04 +0530

    Add files via upload

--- webs/views.py
+++ webs/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render, get_object_or_404
 from .serializers import webSerializer
-# from rest_framework import status
-# from rest_framework.status import HTTP_400_BAD_REQUEST
-# from rest_framework.views import APIView
 from .models import Web
-# from rest_framework.response import Response
-# from django.http import Http404
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate
 from rest_framework import generics
 from rest_framework.permissions import (
         IsAuthenticated,
@@ -13,46 +11,7 @@ from rest_framework.permissions import (
         AllowAny,
         IsAdminUser
 )
-
-'''class WebApi(APIView):
-
-    def get(self, request):
-        webs = Web.objects.all()
-        serializer = webSerializer(webs, many=True)
-        return Response(serializer.data)
-
-    def post(self,request):
-        serializer = webSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class WebApiDetails(APIView):
-    def get_objects(self, pk):
-        try:
-            return  Web.objects.get(pk=pk)
-        except Web.DoesNotExist:
-            return Http404
-
-    def get(self, request, pk):
-        web = self.get_objects(pk)
-        serializer = webSerializer(web)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        web = self.get_objects(pk)
-        serializer = webSerializer(web, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        web = self.get_objects(pk)
-        web.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)'''
+import django_filters.rest_framework
 
 class WebApi(generics.ListCreateAPIView):
     queryset = Web.objects.all()
@@ -63,3 +22,24 @@ class WebApiDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Web.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = webSerializer
+
+class UserListView(generics.ListAPIView):
+    queryset = Web.objects.all()
+    serializer_class = webSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+
+
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/web/')
+            else:
+                messages.error(request, 'Username or password did not match')
+        except ObjectDoesNotExist:
+            print("Invalide user")
+    return render(request, 'webs/login.html')
