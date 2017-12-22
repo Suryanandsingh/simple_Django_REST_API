a2ba68c2b617b847e552e7edf12fc7b45118778a
Suryanand Singh <singhsuryanand@gmail.com>
2017-12-22 12:12:47 +0530

    Add files via upload

--- rest_project/settings.py
+++ rest_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'webs.apps.WebsConfig',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 4
+}
 
 ROOT_URLCONF = 'rest_project.urls'
 
